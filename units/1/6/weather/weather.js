168243d9d59dd33151db47bc961d4a8e2b08e9b3
Arnie Larson <arnie.larson@gmail.com>
2014-03-18 07:10:06 -0700

    adding 1.6.3 stuff for conditions, and updates to 1.6.4

--- units/1/6/weather/weather.js
+++ units/1/6/weather/weather.js
@@ -1,21 +1,37 @@
 
+var weather;
+var season;
 
 /* 
- * This function decides what I will do given the weather
+ * This function decides what I will do today
  */
-function decide(condition) {
-    if (condition=="cloudy") {
-        display("Do homework");
+function decide() {
+    var message;
+    if (weather=="snowy" && season=="winter") {
+        message = "It's a winter wonderland, let's go skiing!!";
+    } else if (weather=="rainy" && season=="winter") {
+        message = "Yuck - I'm playing video games all day";
     } else {
-        display("How should I know?");
+        // This is also the default setting..
+        message = "How should I know?";
     }
+    display(message, "activity")
+}
 
+function selectWeather(input) {
+    weather = input;
+    display("The weather is "+input, "weather");
+}
+
+function selectSeason(input) {
+    season = input;
+    display("The season is "+input, "season");
 }
 
 /*
- *  This function displays a message to the element with id "activity"
+ *  This function displays a message to the an element with specified id
  */
-function display(message) {
-    document.getElementById("activity").textContent = message;
+function display(message, id) {
+    document.getElementById(id).textContent = message;
 }
 
